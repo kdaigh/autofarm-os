1a79ae9f6f54ca61c9b1785edf54cd2c88c4f851
Kristi Daigh <kdaigh20@gmail.com>
2020-02-26 13:14:59 -0600

    Added code and additional example for ph sensor.

--- sensors/sensors.py
+++ sensors/sensors.py
@@ -5,17 +5,23 @@ import time
 liquid_level_pin = 5
 
 # Open serial port
-serPort = '/dev/ttyACM0'
+liquid_level_port = '/dev/ttyACM0'
+ph_port = '/dev/ttyACM1'
 baudRate = 9600
-ser = serial.Serial(serPort, baudRate)
-print("Serial port " + serPort + " opened  Baudrate " + str(baudRate))
+liquid_level_ser = serial.Serial(liquid_level_port, baudRate)
+print("Liquid_level: Serial port " + liquid_level_port + " opened  Baudrate " + str(baudRate))
+ph_ser = serial.Serial(ph_port, baudRate)
+print("PH: Serial port " + ph_port + " opened  Baudrate " + str(baudRate))
 
 # Wait for Arduino
 time.sleep(1)
 
 # Read input from liquid level sensor via Arduino
-read_ser = ser.readline()
-print(read_ser)
+liquid_level_read = liquid_level_ser.readline()
+print(liquid_level_read)
+
+# Read input from ph sensor via Arduino
+
 
 # Close serial port
-ser.close()
+liquid_level_ser.close()
