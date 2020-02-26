332465c1e9eb53840ff6d67d8735953507fbed2c
Kristi Daigh <kdaigh20@gmail.com>
2020-02-26 12:52:18 -0600

    Added experimental code to liquid_level python file.

--- pyserial/liquid_level/liquid_level.py
+++ pyserial/liquid_level/liquid_level.py
@@ -1,5 +1,8 @@
 import serial
+import RPi.GPIO as GPIO
 import time
+
+liquid_level_pin = 5
 
 # Open serial port
 serPort = '/dev/ttyACM0'
@@ -8,11 +11,15 @@ ser = serial.Serial(serPort, baudRate)
 print("Serial port " + serPort + " opened  Baudrate " + str(baudRate))
 
 # Wait for Arduino
-time.sleep(5)
+time.sleep(1)
 
-# Read input from Arduino
-read_ser = ser.readline()
-print(read_ser)
+# Read input from liquid level sensor via Arduino
+GPIO.input(liquid_level_pin)
+time.sleep(1)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(liquid_level_pin, GPIO.IN)
+# read_ser = ser.readline()
+# print(read_ser)
 
 # Close serial port
 ser.close()
