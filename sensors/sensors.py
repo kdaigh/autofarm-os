487f108fc58a07c1166a47e502024f4311323d56
Kristi Daigh <kdaigh20@gmail.com>
2020-02-26 13:36:15 -0600

    Added library for hygrometer. Updated sensors.py.

--- sensors/sensors.py
+++ sensors/sensors.py
@@ -6,11 +6,14 @@ liquid_level_pin = 5
 # Open serial port
 liquid_level_port = '/dev/ttyACM0'
 ph_port = '/dev/ttyACM1'
+hygrometer_port = '/dev/ttyACM2'
 baudRate = 9600
 liquid_level_ser = serial.Serial(liquid_level_port, baudRate)
 print("Liquid_level: Serial port " + liquid_level_port + " opened  Baudrate " + str(baudRate))
 ph_ser = serial.Serial(ph_port, baudRate)
 print("PH: Serial port " + ph_port + " opened  Baudrate " + str(baudRate))
+hygrometer_ser = serial.Serial(ph_port, baudRate)
+print("Hygrometer: Serial port " + hygrometer_port + " opened  Baudrate " + str(baudRate))
 
 # Wait for Arduino
 time.sleep(1)
@@ -23,5 +26,11 @@ print(liquid_level_read)
 ph_read = ph_ser.readline()
 print(ph_read)
 
+# Read input from Hygrometer via Arduino
+hygrometer_read = hygrometer_ser.readline()
+print(hygrometer_read)
+
 # Close serial port
 liquid_level_ser.close()
+ph_ser.close()
+hygrometer_ser.close()
