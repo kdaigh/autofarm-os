6cbd91224e889dadd222a5856bde2af57af71ce7
Kristi Daigh <kdaigh20@gmail.com>
2020-04-20 15:37:19 -0500

    Removed hygrometer check.

--- sensors/hygrometer/hygrometer.ino
+++ sensors/hygrometer/hygrometer.ino
@@ -25,8 +25,7 @@ void setup()
 
     // Init SHT20 Sensor
     sht20.initSHT20();                                 
-    delay(100);      
-    sht20.checkSHT20();                                 // Check SHT20 Sensor                     
+    delay(100);                     
 }
 
 void loop()
